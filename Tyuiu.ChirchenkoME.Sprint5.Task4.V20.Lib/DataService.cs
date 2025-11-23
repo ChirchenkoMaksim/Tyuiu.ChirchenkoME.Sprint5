12a49855754b5ff5815c3d95f0f1672e318144c5
Vlad <Vlad@VLAD_X>
2025-11-24 01:51:08 +0500

    Task4

---
+++ Tyuiu.ChirchenkoME.Sprint5.Task4.V20.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ChirchenkoME.Sprint5.Task4.V20.Lib
+{
+    public class DataService : ISprint5Task4V20
+    {
+        public double LoadFromDataFile(string path)
+        {
+            //Дан файл С:\DataSprint5\InPutDataFileTask4V0.txt (файл взять из архива согласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопировать в неё файл) в котором есть вещественное значение. Прочитать значение из файла и подставить вместо Х в формуле y = (x^2/sin(x))^3 Вычислить значение по формуле (Полученное значение округлить до трёх знаков после запятой) и вернуть полученный результат на консоль.
+            string text = File.ReadAllText(path);
+            double x = double.Parse(text, CultureInfo.InvariantCulture);
+            double y = Math.Pow((Math.Pow(x, 2) / Math.Sin(x)), 3);
+            y = Math.Round(y, 3);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint5.Task4.V20.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ChirchenkoME.Sprint5.Task4.V20.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
