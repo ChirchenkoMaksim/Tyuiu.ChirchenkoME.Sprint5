a6513ec047e809b62fbd874b4a7e488e79a9349f
Vlad <Vlad@VLAD_X>
2025-11-24 00:49:41 +0500

    Add project files.

---
+++ Tyuiu.ChirchenkoME.Sprint5.Task0.V20.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ChirchenkoME.Sprint5.Task0.V20.Lib
+{
+    public class DataService : ISprint5Task0V20
+    {
+        public string SaveToFileTextData(int x)
+        {
+            // Дано выражение f(x) = 2.12x^3 + 1.05x^2 + 4.1x * 2 вычислить его значение при x = 2, результат сохранить в текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до трёх знаков после запятой.
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double result = 2.12 * Math.Pow(x, 3) + 1.05 * Math.Pow(x, 2) + 4.1 * x * 2;
+            result = Math.Round(result, 3);
+            File.WriteAllText(path, result.ToString());
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint5.Task0.V20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.ChirchenkoME.Sprint5.Task0.V20.Lib;
+
+namespace Tyuiu.ChirchenkoME.Sprint5.Task0.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 2;
+
+            Console.WriteLine("x = " + x);
+
+
+            string path = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("File: " + path);
+            Console.WriteLine("Слздан!");
+            Console.ReadKey();
+
+            Console.ReadKey();
+
+        }
+    }
+}
